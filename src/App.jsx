3e9722eae88e81eb46fd6339310a684e0efd415b
Jose roberto Vargas Arango <joserobertovargasarango@MacBook-Pro-de-Jose.local>
2022-12-17 14:13:12 -0500

    cambiando el codigo al componente quoteBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import quote from './quote.json'
-import colors from './colors.json'
+import QuoteBox from './components/QuoteBox'
 
 function App() {
  
-
-
-  // ######## variables ramdom ############
-  const ramdomMesage = Math.floor(Math.random() * quote.length );
-  const ramdomColors = Math.floor(Math.random() * colors.length );
-
-  // ########## variables ##############
-  const quoteMesage = `${quote[ramdomMesage].quote}`;
-  const quoteAutor = `${quote[ramdomMesage].author}`;
-
-  //####### variables usestate ############
-  const [indexCuote, setIndexCuote] = useState(ramdomMesage);
-  const [indexColors, setIndexColors] = useState(ramdomColors);
-
-
-  // ######## Function ####################
-  const ChangeCuote =()=>{
-  const newRamdomMesage = Math.floor(Math.random() * quote.length );
-    setIndexCuote(newRamdomMesage);
-  }
-
-  
-  document.body.style = `background-color: ${colors[ramdomColors].hex}`
-
-
-
   return (
     <div className="App">
-      <div  className='card' style={{color: colors[ramdomColors].hex}}>
-        <div className='quote'>
-          <span className='quote-icon'><i className="fa-solid fa-quote-left"></i></span>
-          <p>{quoteMesage}</p>
-        </div>
-
-        <div className='autor'>
-          <span>{quoteAutor}</span>
-          <button style={{color: colors[ramdomColors].hex}} onClick={ChangeCuote}className='btn-autor'><i className="fa-solid fa-circle-chevron-right"></i></button>
-        </div>
-       
-      </div>
-      <footer className='footer'>&copy; Todos los derechos reservador: José R Vargas</footer>
+      <QuoteBox/>
     </div>
   )
 }
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,50 @@
+import quote from '../quote.json'
+import colors from '../colors.json'
+import { useState } from 'react';
+
+
+const QuoteBox = () => {
+
+  // ######## variables ramdom ############
+  const ramdomMessage = Math.floor(Math.random() * quote.length );
+  const ramdomColors = Math.floor(Math.random() * colors.length );
+
+  // ########## variables ##############
+  const quoteMesage = `${quote[ramdomMessage].quote}`;
+  const quoteAutor = `${quote[ramdomMessage].author}`;
+
+  //####### variables usestate ############
+  const [indexCuote, setIndexCuote] = useState(ramdomMessage);
+  const [indexColors, setIndexColors] = useState(ramdomColors);
+
+
+  // ######## Function ####################
+  const ChangeCuote =()=>{
+  const newRamdomMessage = Math.floor(Math.random() * quote.length );
+    setIndexCuote(newRamdomMessage);
+  }
+
+  document.body.style = `background-color: ${colors[ramdomColors].hex}`
+
+  
+    return (
+        <div>
+             <div  className='card' style={{color: colors[ramdomColors].hex}}>
+        <div className='quote'>
+          <span className='quote-icon'><i className="fa-solid fa-quote-left"></i></span>
+          <p>{quoteMesage}</p>
+        </div>
+
+        <div className='autor'>
+          <span>{quoteAutor}</span>
+          <button style={{color: colors[ramdomColors].hex}} onClick={ChangeCuote}className='btn-autor'><i className="fa-solid fa-circle-chevron-right"></i></button>
+        </div>
+       
+      </div>
+      <footer className='footer'>&copy; Todos los derechos reservador: José R Vargas</footer>
+            
+        </div>
+    );
+};
+
+export default QuoteBox;
